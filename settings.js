69679b5f4989d4845c1adf1b5dc8c188a44b93dd
PersoSirEduard <59216720+PersoSirEduard@users.noreply.github.com>
2020-01-20 20:12:29 -0500

    Add files via upload

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 var settings = {
-	screenWidth:  window.innerWidth,
-	screenHeight: window.innerHeight,
+	screenWidth: window.innerWidth, //Fullscreen width
+	screenHeight: window.innerHeight, //Fullscreen heigth
 	level: 3,
 	maxAliensPerLine: 10
 };
