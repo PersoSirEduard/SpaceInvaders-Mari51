1554d0d13d698cc5c188e978b861078edc6ea4c3
JanPat <46287720+JanPat@users.noreply.github.com>
2020-01-22 10:29:06 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,11 @@
 var settings = {
+	
+	/*
 	screenWidth: window.innerWidth, //Fullscreen width
 	screenHeight: window.innerHeight, //Fullscreen heigth
+	*/
+	screenWidth: 100vw,
+	screenHeight: 100vw,
 	level: 3,
 	maxAliensPerLine: 10
 };
