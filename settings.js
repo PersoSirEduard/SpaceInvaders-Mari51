ec81582421d2f7027632461abb8212d6f802eb03
PersoSirEduard <59216720+PersoSirEduard@users.noreply.github.com>
2020-02-02 15:51:12 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,7 @@
 var settings = {
 	screenWidth: window.innerWidth, //Fullscreen width
 	screenHeight: window.innerHeight, //Fullscreen height
+	fullScreen: true,
 	level: 3,
 	maxAliensPerLine: 10
 };
